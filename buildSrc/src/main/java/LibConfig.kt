c11bb7e1c5bac71ded8e6fceb483f8abcd1b1eaa
Nikita <japskiddin@gmail.com>
2023-02-14 11:50:29 +0800

    * Updated publishing script * Fixed YAML script

--- buildSrc/src/main/java/LibConfig.kt
+++
@@ -1,5 +0,0 @@
-object LibConfig {
-  const val groupId = "io.github.japskiddin"
-  const val artifactId = "debuglogger"
-  const val version = "1.1.2"
-}
--- debuglogger/build.gradle.kts
+++ debuglogger/build.gradle.kts
@@ -1,9 +1,11 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
   id("com.android.library")
   kotlin("android")
+  id("maven-publish")
 }
-
-apply(from = "./publish.gradle")
 
 android {
   namespace = "io.github.japskiddin.debuglogger"
@@ -54,6 +56,73 @@ android {
       )
     }
   }
+}
+
+val propertiesName = "github.properties"
+val githubProperties = Properties()
+if (rootProject.file(propertiesName).exists()) {
+  githubProperties.load(FileInputStream(rootProject.file(propertiesName)))
+}
+
+fun getVersionName(): String {
+  return "1.1.1"
+}
+
+fun getArtificatId(): String {
+  return "debuglogger"
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("debugLogger") {
+      groupId = "io.github.japskiddin"
+      artifactId = getArtificatId()
+      version = getVersionName()
+      artifact("$buildDir/outputs/aar/debuglogger-release.aar")
+    }
+  }
+
+  repositories {
+    maven {
+      name = "GithubPackages"
+      url = uri("https://maven.pkg.github.com/japskiddin/DebugLogger")
+      credentials {
+        username = githubProperties.get("gpr.usr") as String ?: System.getenv("GPR_USER")
+        password = githubProperties.get("gpr.key") as String ?: System.getenv("GPR_API_KEY")
+      }
+    }
+  }
+}
+
+val sourceFiles = android.sourceSets.getByName("main").java.getSourceFiles()
+
+tasks.register<Javadoc>("withJavadoc") {
+  isFailOnError = false
+  dependsOn(tasks.named("compileDebugSources"), tasks.named("compileReleaseSources"))
+
+  // add Android runtime classpath
+  android.bootClasspath.forEach { classpath += project.fileTree(it) }
+
+  // add classpath for all dependencies
+  android.libraryVariants.forEach { variant ->
+    variant.javaCompileProvider.get().classpath.files.forEach { file ->
+      classpath += project.fileTree(file)
+    }
+  }
+
+  source = sourceFiles
+}
+
+tasks.register<Jar>("withJavadocJar") {
+  archiveClassifier.set("javadoc")
+  dependsOn(tasks.named("withJavadoc"))
+  val destination = tasks.named<Javadoc>("withJavadoc").get().destinationDir
+  from(destination)
+}
+
+tasks.register<Jar>("withSourcesJar") {
+  archiveClassifier.set("sources")
+  from(sourceFiles)
 }
 
 dependencies {
